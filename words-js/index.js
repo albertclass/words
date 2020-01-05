2a357808a99fb682bdc0140f130dd6e8fb96b60f
Albert.xu@home <windxu@126.com>
2020-01-05 16:18:59 +0800

    update code

--- words-js/index.js
+++ words-js/index.js
@@ -1,7 +1,122 @@
-const {app, BrowserWindow} = require("electron")
+const { app, BrowserWindow } = require("electron")
 const ipc = require('electron').ipcRenderer
+const fs = require('fs')
+const xpath = require('xpath')
+const http = require('http')
+const sqlite3 = require('sqlite3')
 
 let win
+
+let task = {
+    total : 0,
+    words : {
+
+    }
+}
+
+let dict = new sqlite3.Database("dict.db", (err) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+
+    dict.run('CREATE TABLE IF NOT EXIST "main"."dict"(\
+        "id" integer not null primary key autoincrement,\
+        "word" text,\
+        "symbol" text,\
+        "explain" text,\
+        "example" text,\
+        "en" blob,\
+        "us" blob\
+    );', (res, err) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        console.log(res)
+    })
+
+    dict.run('CREATE TABLE IF NOT EXIST "main"."user"(\
+        "id" integer not null primary key autoincrement,\
+        "username" text,\
+        "password" text,\
+        "nickname" text,\
+        "lastbook" text\
+    );', (res, err) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        console.log(res)
+    })
+
+    
+    dict.run('CREATE TABLE IF NOT EXIST "main"."examine"(\
+        "userid" integer not null,\
+        "bookname" text,\
+        "examine" text,\
+        PARIMARY KEY(userid, bookname)\
+    );', (res, err) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        console.log(res)
+    })
+})
+
+function new_book(user, book, words) {
+    dict.run('replace into \
+    "main"."examine" (userid, bookname, examine) \
+    values (${user}, "${book}", "${words}");', (res, err) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        console.log(res)
+    })
+}
+
+function load_page(word) {
+    const options = {
+        hostname: "cn.bing.com",
+        port: 80,
+        path: "/dict/search?q=" + word,
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    }
+
+    let req = http.request(options, (res) => {
+        if (res.statusCode != 200) {
+            return
+        }
+
+        res.setEncoding('utf8')
+
+        buf = new Buffer()
+        res.on("data", (chunk) => {
+            buf += chunk
+        })
+
+        res.on("end", () => {
+            load_pronunciation(buf)
+            load_explain(buf)
+            load_example(buf)
+        })
+    })
+
+    req.end()
+}
+
+function load(word) {
+
+}
 
 function createWindow() {
     win = new BrowserWindow({
@@ -15,9 +130,11 @@ function createWindow() {
 
     win.loadFile("index.html")
 
-    win.on('ready-to-show', () => {
-      ipc
-    })
+    buf = fs.readFileSync("book.txt")
+    all = buf.toString().split("[\t\n ]")
+    for (word in all) {
+        load(word)
+    }
 
     win.on('close', () => {
         win = null
@@ -31,17 +148,17 @@ app.on('ready', createWindow)
 
 // 当全部窗口关闭时退出。
 app.on('window-all-closed', () => {
-  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
-  // 否则绝大部分应用及其菜单栏会保持激活。
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+    // 否则绝大部分应用及其菜单栏会保持激活。
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
-  // 通常在应用程序中重新创建一个窗口。
-  if (win === null) {
-    createWindow()
-  }
+    // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+    // 通常在应用程序中重新创建一个窗口。
+    if (win === null) {
+        createWindow()
+    }
 })
