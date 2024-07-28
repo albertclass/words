40540cffca25a60eb4efb21485af9fbf94f236cb
xufeng <feng.xu@zixel.cn>
2024-07-29 00:11:26 +0800

    [Update]更新读音爬虫

--- pyword/bdc.py
+++ pyword/bdc.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 import sqlite3
 import logging
+import phonetic
 
 from dataclasses import dataclass, field
 
@@ -37,7 +38,15 @@ class Word:
     bingo: int = 0
     proficiency: int = 0
     content: dict[str,str] = field(default_factory=dict[str,str])
+    sound: pygame.mixer.Sound | None = None
     
+    def play(self):
+        if self.sound is None:
+            filepath = os.path.join(os.path.abspath("./"), self.content["audio"])
+            self.sound = pygame.mixer.Sound(filepath)
+
+        self.sound.play(loops=0)
+        
 class Book:
     '''
     单词书，要背的单词都在单词书里面。会定期进行复习。
@@ -71,6 +80,10 @@ class Book:
         UPDATE {tablename} SET count = count + 1, right = right + 1, proficiency = proficiency + ?, bingo = bingo + 1 where word = ?
     """
     
+    dict_update_audio = """
+        UPDATE stardict SET audio = ? WHERE word = ?
+    """
+    
     def __init__(self, user:str, name:str, dictionary: stardict.StarDict):
         self.dirty = False
         self.words: list[Word] = []
@@ -102,6 +115,13 @@ class Book:
     def isEmpty(self):
         return len(self.words) == 0
     
+    def updateAudio(self, audios: dict[str,str]):
+        connection = sqlite3.connect('dict.db')
+        cursor = connection.cursor()
+        for word, audio in audios.items():
+            cursor.execute(self.dict_update_audio, (audio, word))
+        connection.commit()
+        
     def exists(self) -> bool:
         # Query to check if table exists
         query = "SELECT name FROM sqlite_master WHERE type='table' AND name=?"
@@ -120,14 +140,25 @@ class Book:
             if not self.exists():
                 self.__new()
             
+            yd = phonetic.youdao()
+            
             cursor.execute(self.book_load_query.format(tablename=self.tablename))
+            
+            audios = {}
             for row in cursor.fetchall():
                 w = Word(*row)
                 info = self.dictionary.query(w.word)
                 if info is not None:
+                    if info["audio"] is None or info["audio"] == "":
+                        phoneticPath = yd.down(w.word)
+                        info["audio"] = phoneticPath
+                        # update the word info
+                        audios[w.word] = phoneticPath
+                        
                     w.content = info
                     self.words.append(w)
 
+            self.updateAudio(audios)
         except Exception as e:
             logging.error(f"{type(e)} - {e}")
             return False
@@ -188,11 +219,9 @@ class Letter(pygame.sprite.Sprite):
     '''
     def __init__(self, char: str="", x = 0, y = 0):
         pygame.sprite.Sprite.__init__(self)
-        index = ord(char)
         self.char = char
         self.type = char
         self.last = 0
-        self.texture = image
         self.rect = (x, y, letter_w, letter_h)
         self.set('_')
     
@@ -205,7 +234,7 @@ class Letter(pygame.sprite.Sprite):
             self.type = char
 
             ch -= ord(' ')
-            self.image = self.texture.subsurface((ch * letter_w, 0, letter_w, letter_h))
+            self.image = letters_image.subsurface((ch * letter_w, 0, letter_w, letter_h))
 
     def reset(self):
         self.set('_')
@@ -217,16 +246,18 @@ class Letter(pygame.sprite.Sprite):
         return self.type == self.char
 
 class CharSequence(pygame.sprite.Group):
-    def __init__(self, word, x, y):
+    def __init__(self, word, size: tuple[int, int]):
         pygame.sprite.Group.__init__(self)
         self.sequence: list[Letter] = []
-        self.x = x - len(word) * (letter_w + 1) / 2
-        self.y = y - letter_h / 2
         self.judge = False
         self.complate = False
 
+        self.x = (size[0] - len(word) * (letter_w + 1) - 1) // 2
+        self.y = (size[1] - letter_h - 60)
+
+        x = self.x
         for ch in word:
-            self.sequence.append(Letter(ch, x, y))
+            self.sequence.append(Letter(ch, x, self.y))
             x += letter_w + 1
 
         # add to group
@@ -308,10 +339,10 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600), 0, 32)
 # load font to display
 fonts = [
-    pygame.font.SysFont(["Microsoft YaHei", "SimHei", "Lucida Sans Unicode", "dejavusans"], 20),
+    pygame.font.SysFont(["Microsoft YaHei", "SimHei", "Lucida Sans Unicode", "dejavusans"], 24),
     pygame.font.SysFont("SimHei", 32), # Arial,Helvetica,Sans-Serif
     pygame.font.SysFont("Calibri", 24),
-    pygame.font.SysFont("Microsoft YaHei", 24),
+    pygame.font.SysFont("Microsoft YaHei", 32),
 ]
 
 # Calc the letter weight and height
@@ -319,10 +350,10 @@ letters=""
 for i in range(32, 126):
     letters += chr(i)
 
-image = fonts[1].render(letters, True, [255,255,255])
-image_rect = image.get_rect()
-letter_w = image_rect.width / 94
-letter_h = image_rect.height
+letters_image = fonts[1].render(letters, True, [255,255,255])
+letters_image_rc = letters_image.get_rect()
+letter_w = letters_image_rc.width // 94
+letter_h = letters_image_rc.height
 
 # load book and prepare the data
 b = Book('xuchenhao', 'm1u1', dictionary)
@@ -337,12 +368,11 @@ pygame.key.stop_text_input()
 
 # 开始背单词
 for w in b:
-    s = CharSequence(w.word, 400, 540)
-    w.count += 1
-    factor = 0.1
-
+    s = CharSequence(w.word, screen.get_size())
     g = pygame.sprite.Group()
+    # show the phonetic
     g.add(Sprite(fonts[2].render(w.content["phonetic"], True, [255,255,255]), 10, 40))
+    # show the explain
     if "translation" in w.content \
         and w.content["translation"] is not None \
         and type(w.content["translation"]) is str:
@@ -351,10 +381,6 @@ for w in b:
         for i, translation in enumerate(translations):
             g.add(Sprite(fonts[0].render(translation, True, [255,255,255]), 10, 70 + i * 30))
 
-    # mp3 = "./pronunciation/" + sound + "/" + w.word + ".mp3"
-    # if os.path.exists(mp3):
-    #     pygame.mixer.music.load(mp3)
-    #     pygame.mixer.music.play()
     wrong = 0
     while running and not s.complated():
         tick = pygame.time.get_ticks()
@@ -379,14 +405,18 @@ for w in b:
                         b.wrong(w)
                         s.reset()
                         
-                    if wrong > 2:
-                        # play the pronunciation
-                        pass
-                    
-                    if wrong > 5:
-                        # show correct spelling
-                        g.add(Sprite(fonts[0].render(w.word, True, [200,0,100]), 10, 10))
-                        break
+                        if wrong > 2:
+                            # play the pronunciation, if wrong more than 2 times
+                            w.play()
+                        
+                        if wrong > 4:
+                            # show correct spelling, if wrong more than 5 times
+                            answer = fonts[3].render(w.word, True, [200,0,100])
+                            answer_rect = answer.get_rect()
+                            pos_x = (800 - answer_rect.width) // 2
+                            pos_y = (600 - answer_rect.height) // 2
+                            g.add(Sprite(answer, pos_x, pos_y))
+                            break
                 else:
                     s.press(event.key)
 
--- pyword/phonetic.py
+++ pyword/phonetic.py
@@ -5,10 +5,11 @@
 '''
 
 import os
+import sqlite3
 import urllib.request
-
+import time
 class youdao():
-    def __init__(self, type: int = 0, rootpath: str = './'):
+    def __init__(self, type: int = 1, rootpath: str = './'):
         '''
         调用youdao API
         type = 0：美音
@@ -20,66 +21,121 @@ class youdao():
         self._type = type  # 发音方式
 
         # 文件根目录
-        self._dirRoot = os.path.dirname(os.path.abspath(rootpath))
+        self._dirRoot = os.path.abspath(rootpath)
         if 0 == self._type:
-            self._dirSpeech = os.path.join(self._dirRoot, 'us')  # 美音库
+            self._dirSpeech = os.path.join(self._dirRoot, "phonetic", 'us')  # 美音库
         else:
-            self._dirSpeech = os.path.join(self._dirRoot, 'en')  # 英音库
+            self._dirSpeech = os.path.join(self._dirRoot, "phonetic", 'en')  # 英音库
 
         # 判断是否存在美音库
-        if not os.path.exists(self._dirRoot):
+        if not os.path.exists(self._dirSpeech):
             # 不存在，就创建
-            os.makedirs(self._dirRoot)
+            os.makedirs(self._dirSpeech)
 
-    def down(self, word):
+    def down(self, word) -> str | None:
         '''
         下载单词的MP3
         判断语音库中是否有对应的MP3
         如果没有就下载
         '''
         word = word.lower()  # 小写
-        tmp = self._getWordMp3FilePath(word)
-        if tmp is None:
-            urlpath = 'http://dict.youdao.com/dictvoice?type=' + str(self._type) + '&audio=' + self._word
+        if not word.isprintable():
+            return None
+        
+        word = word.replace(' ', '+')
+        word = word.replace('.', '')
+       # split word to list, each two letters a group
+        subdirs = []
+        if len(word) > 0:
+            subdirs.append(word[0])
+            
+        if len(word) > 1:
+            subdirs.append(word[1:3])
+            
+        if len(word) > 3:
+            subdirs.append(word[3:7])
+            
+        if len(word) > 7:
+            subdirs.append(word[7:12])
+        
+        filepath_old = os.path.join(self._dirSpeech, *subdirs[:2], word[7:], f"{word}.mp3")
+        filepath = os.path.join(self._dirSpeech, *subdirs, f"{word}.mp3")
+        
+        if os.path.exists(filepath_old):
+            # 如果存在旧文件，就移动到新文件夹
+            os.rename(filepath_old, filepath)
+            # 删除空文件夹
+            if os.path.exists(os.path.dirname(filepath_old)) and len(os.listdir(os.path.dirname(filepath_old))) == 0:
+                os.rmdir(os.path.dirname(filepath_old))
+                
+        if not os.path.exists(filepath):
+            # 如果不存在，就下载
+            if os.path.exists(os.path.dirname(filepath)) == False:
+                # 如果目录不存在，就创建
+                os.makedirs(os.path.dirname(filepath))
+                
+            urlpath = 'http://dict.youdao.com/dictvoice?type=' + str(self._type) + '&audio=' + word.replace('+', '%20')
             # 调用下载程序，下载到目标文件夹
             # print('不存在 %s.mp3 文件\n将URL:\n' % word, self._url, '\n下载到:\n', self._filePath)
             # 下载到目标地址
-            urllib.request.urlretrieve(urlpath, filename=tmp)
-            print('%s.mp3 下载完成' % word)
+            urllib.request.urlretrieve(urlpath, filename=filepath)
+            print(f"下载完成 {word}.mp3, 保存位置：{filepath}")
         else:
-            print('已经存在 %s.mp3, 不需要下载' % self._word)
+            print(f'已经存在 {word}.mp3, 保存位置：{filepath}')
+
+        if os.path.getsize(filepath) == 13:
+            delete = False
+            with open(filepath, 'rb') as f:
+                if f.read() == b'{\"code\": 403}':
+                    delete = True
+            
+            if delete:
+                print(f'删除无效文件 {filepath}\n下载地址：{urlpath}')
+                os.remove(filepath)
+                
+                time.sleep(5)
 
         # 返回声音文件路径
-        return self._filePath
-
-    def _getURL(self):
-        '''
-        私有函数，生成发音的目标URL
-        http://dict.youdao.com/dictvoice?type=0&audio=
-        '''
-        self._url = 
-
-    def _getWordMp3FilePath(self, word):
-        '''
-        获取单词的MP3本地文件路径
-        如果有MP3文件，返回路径(绝对路径)
-        如果没有，返回None
-        '''
-        word = word.lower()  # 小写
-        self._word = word
-        self._fileName = self._word + '.mp3'
-        self._filePath = os.path.join(self._dirSpeech, self._fileName)
-
-        # 判断是否存在这个MP3文件
-        if os.path.exists(self._filePath):
-            # 存在这个mp3
-            return self._filePath
-        else:
-            # 不存在这个MP3，返回none
-            return None
-
+        return os.path.relpath(filepath, self._dirRoot)
 
 if __name__ == "__main__":
-
     sp = youdao()
-    sp.down('reconcile')
+    connection = sqlite3.connect('dict.db')
+    cursor = connection.cursor()
+    
+    dict_select_audio = """
+        SELECT word, audio FROM stardict
+    """
+    
+    dict_update_audio = """
+        UPDATE stardict SET audio = ? WHERE word = ?
+    """
+    
+    cursor.execute(dict_select_audio)
+    old = time.time()
+    word_count = 0
+    save_count = 0
+    rows = cursor.fetchall()
+    for row in rows:
+        word = row[0]
+        if word.isalpha() == False:
+            word_count += 1
+            print(f"跳过 {word}, 不是单词")
+            continue
+        
+        audio = sp.down(word)
+        if audio is not None and audio != row[1]:
+            cursor.execute(dict_update_audio, (audio, word))
+            
+        word_count += 1
+        if time.time() - old > 60:
+            connection.commit()
+            print(f"等待 5 秒 ..., 已经下载 {word_count}/{len(rows)} 个单词")
+            time.sleep(10)
+            old = time.time()
+        
+    connection.commit()
+    connection.close()
+    
+
+    
